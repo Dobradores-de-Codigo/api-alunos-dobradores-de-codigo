51f6a497f957158055c1b966a41e7b893d04d565
Jeferson5641 <jefe.gomes154@gmail.com>
2024-05-10 13:58:00 -0300

    Projeto inicial para o ambiente de trabalho

---
+++ src/test/java/com/example/alunos/AlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
